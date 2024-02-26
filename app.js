f221d1ca49ab1c5fd8c9dcf78493a04d3f5a9c96
Pratik Pansare <84313956+pratik97669966@users.noreply.github.com>
2024-02-26 22:31:14 +0530

    k

--- app.js
+++ app.js
@@ -17,15 +17,14 @@ client.connect()
     .then(() => {
         console.log('Connected to MongoDB');
         app.locals.db = client.db('HomeScreen'); // Specify the database name here app.use(logger('dev'));
-
-    })
-    .catch(err => {
-        console.error('Error connecting to MongoDB:', err);
-        process.exit(1); // Exit the application if MongoDB connection fails
-    });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', quotesRouter);
 module.exports = app;
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err);
+        process.exit(1); // Exit the application if MongoDB connection fails
+    });
