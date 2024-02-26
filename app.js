5f93de5d90a9b408bbf2a4b8222764154b4bb82a
Pratik Pansare <84313956+pratik97669966@users.noreply.github.com>
2024-02-26 22:28:58 +0530

    ll

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const quotesRouter = require('./routes/quotes');
 const app = express();
 
 // MongoDB connection URI
-const uri = 'mongodb+srv://root:root@telusko.rb3lafm.mongodb.net/HomeScreen';
+const uri = 'mongodb+srv://root:root@telusko.rb3lafm.mongodb.net/?retryWrites=true&w=majority&appName=telusko';
 
 // Connect to MongoDB and store the connection in app.locals
 const client = new MongoClient(uri);
@@ -17,14 +17,15 @@ client.connect()
     .then(() => {
         console.log('Connected to MongoDB');
         app.locals.db = client.db('HomeScreen'); // Specify the database name here app.use(logger('dev'));
-        app.use(express.json());
-        app.use(express.urlencoded({ extended: false }));
-        app.use(cookieParser());
-        app.use(express.static(path.join(__dirname, 'public')));
-        app.use('/', quotesRouter);
-        module.exports = app;
+
     })
     .catch(err => {
         console.error('Error connecting to MongoDB:', err);
         process.exit(1); // Exit the application if MongoDB connection fails
     });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', quotesRouter);
+module.exports = app;
