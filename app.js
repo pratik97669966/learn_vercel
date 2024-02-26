8d0a442f1d500f708e41ca43b834930ce49406a0
Pratik Pansare <84313956+pratik97669966@users.noreply.github.com>
2024-02-26 22:11:17 +0530

    ii

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const { MongoClient } = require('mongodb');
 
-var indexRouter = require('./routes/index');
-var quotesRouter = require('./routes/quotes');
+// const indexRouter = require('./routes/index');
+const quotesRouter = require('./routes/quotes');
 
-var app = express();
+const app = express();
+
+// MongoDB connection URI
+const uri = 'mongodb+srv://root:root@telusko.rb3lafm.mongodb.net/';
+
+// Connect to MongoDB and store the connection in app.locals
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect()
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.locals.db = client.db('HomeScreen');
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err);
+    });
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/quotes', quotesRouter);
 
 module.exports = app;
--- routes/quotes.js
+++ routes/quotes.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const quotes = require('../services/quotes');
 
-/* GET quotes listing. */
-router.get('/', async function(req, res, next) {
-  try {
-    res.json(await quotes.getMultiple(req.query.page));
-  } catch (err) {
-    console.error(`Error while getting quotes `, err.message);
-    res.status(err.statusCode || 500).json({'message': err.message});
-  }
+// GET all quotes
+router.get('/', async (req, res) => {
+  const collection = req.app.locals.db.collection('instanttalk');
+  const quotes = await collection.find({}).toArray();
+  res.json(quotes);
 });
 
 /* POST quotes */
-router.post('/', async function(req, res, next) {
+router.post('/', async function (req, res, next) {
   try {
     res.json(await quotes.create(req.body));
   } catch (err) {
     console.error(`Error while posting quotes `, err.message);
-    res.status(err.statusCode || 500).json({'message': err.message});
+    res.status(err.statusCode || 500).json({ 'message': err.message });
   }
 });
 
