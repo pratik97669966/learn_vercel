769cd806d79ac8a01e87708f87e3096fdb8031c7
Pratik Pansare <84313956+pratik97669966@users.noreply.github.com>
2024-02-27 02:30:34 +0530

    renderer

--- app.js
+++ app.js
@@ -3,29 +3,31 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { MongoClient } = require('mongodb');
-
-const quotesRouter = require('./routes/quotes');
-
 const app = express();
-
+const server = require("http").createServer(app);
+const quotesRouter = require('./routes/quotes');
+const PORT = process.env.PORT || 3030;
 // MongoDB connection URI
 const uri = 'mongodb+srv://dreamercloudofficial:dreamercloudofficial@rooms.drpj12s.mongodb.net/?retryWrites=true&w=majority&appName=Rooms';
-
 // Connect to MongoDB and store the connection in app.locals
 const client = new MongoClient(uri);
 client.connect()
     .then(() => {
         console.log('Connected to MongoDB');
         app.locals.db = client.db('test');
-   
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use('/', quotesRouter);
-module.exports = app;
- })
+
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: false }));
+        app.use(cookieParser());
+        app.use(express.static(path.join(__dirname, 'public')));
+        app.use('/', quotesRouter);
+        module.exports = app;
+
+    })
     .catch(err => {
         console.error('Error connecting to MongoDB:', err);
         process.exit(1); // Exit the application if MongoDB connection fails
     });
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
